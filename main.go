ba6754abc57ad7dd61f355d69227cc1bd0f20916
micodee <martomhidayat>
2023-02-04 11:45:21 +0700

    create add detail

--- main.go
+++ main.go
@@ -16,6 +16,46 @@ var data = map[string]interface{}{
 	"isLogin" : true,
 }
 
+type postproject struct {
+	Title string
+	postdate string
+	Durasi string
+	Content string
+}
+var Projects = []postproject{
+	{
+		Title : "Pasar Coding di Indonesia Dinilai Masih Menjanjikan",
+		postdate : "12 Jul 2021 22:30 WIB",
+		Durasi : "1 Bulan",
+		Content : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor provident culpa magni eum ab voluptatum, iste error, aut voluptas officiis odio tempora reprehenderit quos voluptates mollitia explicabo. Dolores, tempore expedita?",
+	},
+	{
+		Title : "Disini masih ada",
+		postdate : "12 Jul 2021 22:30 WIB",
+		Durasi : "2 Minggu",
+		Content : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor provident culpa magni eum ab voluptatum, iste error, aut voluptas officiis odio tempora reprehenderit quos voluptates mollitia explicabo. Dolores, tempore expedita?",
+	},
+	{
+		Title : "lalalaaa",
+		postdate : "12 Jul 2021 22:30 WIB",
+		Durasi : "6 Hari",
+		Content : "App that used for dumbways student, it was deployed and can downloaded on playstore. Happy download",
+	},
+	{
+		Title : "Hallo World",
+		postdate : "12 Jul 2021 22:30 WIB",
+		Durasi : "3 Hari",
+		Content : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor provident culpa magni eum ab voluptatum, iste error, aut voluptas officiis odio tempora reprehenderit quos voluptates mollitia explicabo. Dolores, tempore expedita?",
+	},
+	{
+		Title : "bai bfdsl ksssu ldjkacau",
+		postdate : "12 Jul 2021 22:30 WIB",
+		Durasi : "5 Bulan",
+		Content : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor provident culpa magni eum ab voluptatum, iste error, aut voluptas officiis odio tempora reprehenderit quos voluptates mollitia explicabo. Dolores, tempore expedita?",
+	},
+	}
+
+
 func main() {
 	// menyiapkan routingan
 	router := mux.NewRouter()
@@ -24,21 +64,14 @@ func main() {
 	router.PathPrefix("/public").Handler(http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
 
 	// routing
-	router.HandleFunc("/hello", hello).Methods("GET")
 	router.HandleFunc("/", Home).Methods("GET")
 	router.HandleFunc("/project", Project).Methods("GET")
-	router.HandleFunc("/contact", Contact).Methods("GET")
 	router.HandleFunc("/detail/{id}", ProjectDetail).Methods("GET")
 	router.HandleFunc("/addProject", AddProject).Methods("POST")
+	router.HandleFunc("/contact", Contact).Methods("GET")
 
 	fmt.Println("Server Running on port 5000")
 	http.ListenAndServe("localhost:5000", router)
-}
-
-func hello (w http.ResponseWriter, req *http.Request)  {
-	w.Header().Set("Content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("hello world"))
 }
 
 
@@ -57,8 +90,13 @@ func Home (w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	resp := map[string]interface{}{
+		"Data" : data,
+		"Projects" : Projects,
+	}
+
 	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w, data)
+	tmpl.Execute(w, resp)
 }
 
 // function handling project
@@ -66,7 +104,7 @@ func Project (w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-type", "text/html; charset-utf-8")
 
 	// parsing template html
-	var tmpl, err = template.ParseFiles("./views/my-project.html")
+	var tmpl, err = template.ParseFiles("./views/projectAdd.html")
 	// error handling
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -103,7 +141,7 @@ func ProjectDetail (w http.ResponseWriter, req *http.Request) {
 	id, _ := strconv.Atoi(mux.Vars(req)["id"])
 
 	// parsing template html
-	var tmpl, err = template.ParseFiles("./views/detail-project.html")
+	var tmpl, err = template.ParseFiles("./views/projectDetail.html")
 	// error handling
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -127,8 +165,15 @@ func AddProject (w http.ResponseWriter, req *http.Request) {
 		log.Fatal(err)		
 	}
 
-	fmt.Println("Project Name : " + req.PostForm.Get("pname"))
-	fmt.Println("Description : " + req.PostForm.Get("desc"))
+	title := req.PostForm.Get("pname")
+	content := req.PostForm.Get(("desc"))
+
+	var newProject = postproject{
+		Title: title,
+		Content: content,
+	}
+
+	Projects = append(Projects, newProject)
 
 	http.Redirect(w, req, "/", http.StatusMovedPermanently)
 }
